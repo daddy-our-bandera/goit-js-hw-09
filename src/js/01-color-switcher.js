d161df15525ea7b104a876e4a190457a11b2d997
Vlad-Kolyada <107812458+daddy-our-bandera@users.noreply.github.com>
2022-11-29 15:33:39 +0200

    ex-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const btnStart = document.querySelector("[data-start]");
+const btnStop = document.querySelector("[data-stop]");
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
+
+  let timerId = null;
+
+btnStart.addEventListener("click",() => {
+    timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor()},1000)
+    btnStart.setAttribute("disabled",true)
+    
+})
+
+btnStop.addEventListener("click",() => {
+    clearInterval(timerId);
+    btnStart.removeAttribute("disabled")
+})
